f472a6316bfe3176c34b46c7e212cdcef6f1bc3e
liyangworld <1909457483@qq.com>
2017-09-29 10:53:17 +0800

    新增命令行功能
    新增命令行功能

---
+++ bin/cli.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var logger = require('../logger.js');
+
+program.version('v'+require('../package.json').version)
+		.description('npm包对应的命令行工具。');
+
+program.command('log <msg> [otherMsgs...]')
+		.alias('l')
+		.description('打印输入的参数')
+		.action((msg,otherMsgs)=>{
+			let str = msg + otherMsgs.join(' ');
+			logger.log(str);
+
+		})
+
+program.parse(process.argv);
+
+if (program.args.length === 0) {  
+  program.help()  
+}  
